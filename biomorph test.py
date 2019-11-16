a19d7584e8598c2d3acb0d14f1d362fad6415766
Jim Marshall <jimmarshall35@gmail.com>
2019-11-16 15:26:06 +0000

    Update biomorph test.py

--- biomorph test.py
+++ biomorph test.py
@@ -3,50 +3,77 @@ import random
 import pygame.math
 win = pygame.display.set_mode((1000,500))
 pygame.init()
+defaultanglemag = 30
+defaultlength = 20
+defaultdepth = 10
+
+maxlengthchange = 28
+maxanglechange = 180
 
 class Node(object):
     
     def __init__(self, pos, depth, parent):
-        self.set_pos(pos)
+        self.pos = pos
         self.depth = depth
         self.parent = parent
-    def set_pos(self,pos):
-        self._pos = pos
-    def get_pos(self):
-        return self._pos
-    
-def addNodePair(i, node, length, anglemagnitude):
-    LorR = 1 # is left or right branch?
-    for x in range(2):
-        vector = node.get_pos() - node.parent.get_pos()
-        normalizedvector = vector.normalize()
-        rotatedvector = normalizedvector.rotate(anglemagnitude*LorR)
-        newpos = node.get_pos() + rotatedvector*length
-        newnode = Node(newpos, i, node)
-        pygame.draw.line(win, Color_line, (node.get_pos()), (newnode.get_pos()))
-        LorR *= -1
-        pygame.display.flip() # update screen to show drawn lines
-        nodes.append(newnode)
         
+    def addNodePair(self, i, biomorph):
+        # this term is -1 the second iteration of the for loop - so that the two branches have reflectional symmetry in y axis
+        LorR = 1 
+        # get normalized vector from this node to its parent.
+        # this will be used to generate child node positions by rotating it, changing its magnitude and then adding to the nodes position.
+        normalizedvector = (self.pos - self.parent.pos).normalize()
+        #generate two child nodes and draw line from this node to each one - rotating by equal amount in first positive then negative direction
+        for x in range(2):
+            k1 = maxanglechange/9
+            k2 = (maxlengthchange+1)/9
+            fraction = i/biomorph.genome[0]
+            rotationamount = defaultanglemag+biomorph.genome[1]*k1 + biomorph.genome[2]*fraction*k1
+            magnitude = defaultlength + biomorph.genome[3]*k2 + biomorph.genome[4]*fraction*k2
+            
+            rotatedvector = normalizedvector.rotate(rotationamount*LorR)
+            
+            newpos = self.pos + rotatedvector* magnitude
+            newnode = Node(newpos, i, node)
+            pygame.draw.line(win, Color_line, (node.pos), (newnode.pos))
+            LorR *= -1
+            
+            biomorph.nodes.append(newnode)
+        pygame.display.flip() # update screen to show drawn lines   
+class BioMorph(object):
+    
+    def __init__(self, genome):
+        self.genome = genome
+        self.nodes = []
+            
 treenumber = 15
-run = True
+run = True  # loop to test different parameters
 while(run):
     for x in range(15): # draws 15 trees
-        nodes = []
-        Color_line = (random.randrange(0,255),random.randrange(0,255),random.randrange(0,255))
-        anglemagnitude = random.randrange(0,360)
-        length = random.randrange(2,30)
-        x= (x+1)*(1000/treenumber)
+        #nodes = []
+        biomorph = BioMorph((
+                             random.randrange(-9,9),
+                             random.randrange(-9,9),
+                             random.randrange(-9,9),
+                             random.randrange(-9,9),
+                             random.randrange(-9,9)
+                            ))
+        print(biomorph.genome)
+        Color_line = (random.randrange(0,255),random.randrange(0,255),random.randrange(0,255)) 
+        anglemagnitude = 30
+        length = 20
+        
+        x= (x+1)*(1000/treenumber) # set the inital starting point of the tree
         y= 300
         base = Node(pygame.math.Vector2(x,y),0,None)
-        startnode = Node(pygame.math.Vector2(x,y-20), 0, base)
-        pygame.draw.line(win,Color_line, base.get_pos(), startnode.get_pos())
+        startnode = Node(pygame.math.Vector2(x,y+biomorph.genome[3]*((maxlengthchange)/9)+1), 0, base)
+        pygame.draw.line(win,Color_line, base.pos, startnode.pos)
         pygame.display.flip()
-        nodes.append(startnode)
-        for i in range(1,6):  # recursion depth of 6
-            for node in nodes:
+        biomorph.nodes.append(startnode)
+        for i in range(1, abs(biomorph.genome[0])+1):  # recursion depth of 6
+            for node in biomorph.nodes:
                 if node.depth == i-1:
-                    addNodePair(i, node, length,anglemagnitude)
+                    node.addNodePair(i, biomorph)#length,anglemagnitude)
 
     win.fill((0,0,0))
     
